e15878d5039af0fc5cfe18a5deb9183e96a88a3b
ZhengXianda <xianda.zheng@jiamiantech.com>
2022-03-10 11:01:21 +0800

    添加源码

---
+++ TKCPHuman/Classes/TKCPHuman.swift
@@ -0,0 +1,14 @@
+//
+//  TKCPHuman.swift
+//  Pods-TKCPHuman_Example
+//
+//  Created by ZhengXianda on 2022/3/9.
+//
+
+import UIKit
+
+public class TKCPHuman: NSObject {
+    public static func intro() -> String {
+        return "Human"
+    }
+}
